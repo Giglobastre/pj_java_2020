951eecceafca056f48beb4d62ad2758b3cf11686
Kenny HUBER <36986213+Giglobastre@users.noreply.github.com>
2020-06-07 16:08:01 +0200

    comment

---
+++ src/CONTROLER/CONTROLER/Controler_main.java
@@ -0,0 +1,32 @@
+package CONTROLER;
+import MODEL.*;
+import VIEW.*;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+/**
+ *
+ * @author Kenny-portable
+ */
+public class Controler_main {
+    /**
+         * controler main
+         *
+         * @param args ignored
+         */
+    public static void main(String[] args) {
+        Connexion co=new Connexion();
+        
+        Fenetre_Connexion fco=new Fenetre_Connexion(co);//Cree une fenetre de connexion
+}
+
+
+
+
+}
+
+
+
+
